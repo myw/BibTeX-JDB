d10a36cc13f1bded17ceb2d6fdb0981be6a57f47
Misha Wolfson <mywolfson@gmail.com>
2011-04-25 19:54:33 -0400

    Improve user-friendliness
    * Better options structure
    * Usage info

--- get_journals.sh
+++ get_journals.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 #
-awk -F= '/Journal =/{ gsub("[{},]", "", $2); sub(" ", "", $2); print $2 }' < $1 | sort -u
+
+usage () {
+	echo "$0 <.bib file> - get a list of all the journal names in a .bib file"
+	exit 1
+}
+
+
+if [ -z "$1" ]; then
+	usage
+else
+	awk -F= '/Journal =/{ gsub("[{},]", "", $2); sub(" ", "", $2); print $2 }' < $1 | sort -u
+fi
+
--- make_macros.sh
+++ make_macros.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+usage () {
+	echo "$0 -[hfns] <journal abbreviation database>"
+	echo -e "\n\tDefaults: .bib-style macro, abbreviated journal name, dots"
+	echo -e "\t\te.g. Journal of Physical Chemistry -> J. Chem. Phys."
+	echo -e "\n\t-h - print this help message"
+	echo -e "\t-f - use the full journal name, not an abbreviation [full]"
+	echo -e "\t-n - do not use dots [nodots]"
+	echo -e "\t-s - make a .bst-style macro instead [style]"
+	exit 1
+}
+
 field=2
 nodot=0
-left='MACRO{'
-middle='} {\"'
+left='@string{'
+middle=' = \"'
 
-while getopts fns opt; do
+while getopts fhns opt; do
 	case $opt in
+		h)
+			usage
+			;;
 		f)
 			field=3
 			;;
@@ -14,18 +28,22 @@ while getopts fns opt; do
 			nodot=1
 			;;
 		s)
-			left='@string{'
-			middle=' = \"'
+			left='MACRO{'
+			middle='} {\"'
 			;;
 	esac
 
 done
 shift $(($OPTIND - 1))
 
-awk -v field=$field -v nodot=$nodot -v left="$left" -v middle="$middle" -F'|' '
-	{	if (nodot && field == 2){ 
-			gsub("\\.", "", $field) 
+if [ -z "$1" ]; then
+	usage
+else
+	awk -v field=$field -v nodot=$nodot -v left="$left" -v middle="$middle" -F'|' '
+		{	if (nodot && field == 2){ 
+				gsub("\\.", "", $field) 
+			}
+			print left $1 middle $field "\"}" 
 		}
-		print left $1 middle $field "\"}" 
-	}
-' $@ | sort -u 
+	' $@ | sort -u 
+fi
